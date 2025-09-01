4de54ee8f9d4f7cf09aeda96e00b9a27c17a26ae
Vasugoli <golivasu7@gmail.com>
2025-09-01 18:24:47 +0530

    Add showkeys and trouble.nvim plugins with configuration

--- lua/plugins/errorline.lua
+++ lua/plugins/errorline.lua
@@ -1,4 +1,4 @@
-return {
+return {{
     "rachartier/tiny-inline-diagnostic.nvim",
     event = "VeryLazy",
     priority = 1000,
@@ -142,4 +142,39 @@ return {
             disabled_ft = {}
         })
     end
-}
+}, {
+    "folke/trouble.nvim",
+    event = "VeryLazy",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {{
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)"
+    }, {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)"
+    }, {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)"
+    }, {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)"
+    }, {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)"
+    }, {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)"
+    }},
+    config = function()
+        require("trouble").setup({
+            -- Your Trouble configuration options go here
+        })
+    end
+}}
---
+++ lua/plugins/showkeys.lua
@@ -0,0 +1,45 @@
+return {{
+    "nvzone/showkeys",
+    cmd = "ShowkeysToggle",
+    opts = {
+        position = "bottom-right",
+        maxkeys = 3,
+        show_count = true,
+        winopts = {
+            focusable = false,
+            relative = "editor",
+            style = "minimal",
+            rounded = true,
+            border = "single",
+            height = 1,
+            row = 1,
+            col = 0
+        }
+    }
+}, {{
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        style = "helix",
+        border = "single",
+        layout = {
+            height = { min = 4, max = 25 },
+            width = { min = 20, max = 50 },
+            spacing = 3,
+            align = "left"
+        }
+    },
+    keys = {{
+        "<leader>",
+        function()
+            require("which-key").show({
+                global = true
+            })
+        end,
+        desc = "Buffer Local Keymaps (which-key)"
+    }}
+}}}
